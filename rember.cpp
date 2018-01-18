e946fab908bf642a85b4afc5c288ffd37df0328e
hxchi <981863673@qq.com>
2018-01-18 15:46:43 +0800

    rember code 2.0, can choose the level by player.

--- rember.cpp
+++ rember.cpp
@@ -1,8 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+
+
+#define MAXSTAGE 10
+#define MIMLEVEL 3
+#define MAXLEVEL 20
 
 // function for paste x ms
+int sleep(unsigned long x); 
+void getnumber(int level, char a[42]);
+void getnumberP(int level, char a[42]);
+
+int main()
+{
+	clock_t	start, end; 
+	srand(time(NULL)); 
+
+	char a[42];
+	char b[42];
+	int c;
+	int m = 0,n = 1;
+
+	start = clock();
+	while (1) {
+		m++;
+		printf("choose a level from 3 to 20: ");
+		scanf("%d", &c);
+		getnumber(c, a);
+		printf("%s", a);
+		fflush(stdout);
+		sleep(1000);
+
+		printf("now please enter you answer: ");
+		scanf("%s", b);
+		printf("%s\n", b);
+		if (a == b) {
+			printf("you are right...\n");
+			n++;
+		}
+		if (m == 3) {
+			printf("game over...\n");
+			break;
+		}
+	}
+	end = clock();
+
+	printf("you get %d right...\n", n);
+	printf("use %.1fs...\n", (double)(end-start)/CLOCKS_PER_SEC);
+	
+	// test number
+	// getnumber(20, a);
+	// for (int i = 0; i < strlen(a); i++) {
+	// 	printf("%c", a[i]);
+	// }
+	// printf("\n");
+	
+	return 0;
+}
+
+
+/*-------------------------------------------------------------------*/
 int sleep(unsigned long x) 
 {
 	clock_t c1 = clock(), c2;
@@ -14,36 +73,19 @@ int sleep(unsigned long x)
 }
 
 
-int main()
+void getnumber(int level, char a[42])
 {
-	clock_t	start, end; // for calculate the time it takes
-	srand(time(NULL)); // random seed
-	int a, b; // right answer, player answer
-	int m = 0, n = 0; // total times and right times
-
-	start = clock();
-	while (1) {
-		a = rand() % 900 + 1000;
-		printf("%d", a);
-		fflush(stdout);
-
-		sleep(500);
-		// printf("\b\b\b\b    \b\b\b\b\n");
-		printf("\renter you number: ");
-		scanf("%d", &b);
-		n++;
-		if (b == a) {
-			m++;
+	int c;
+	for(int i = 0; i < level; i++) {
+		if (i == 0) {
+			c = (rand()%9) + 1;
+			sprintf(&a[0], "%d", c);
 		}
-		if (n == 3) {
-			printf("%d tims you are right.\n", m);
-			break;
+		else {
+			c = rand()%10;
+			sprintf(&a[i], "%d", c);
 		}
 	}
-	end = clock();
-
-	printf("time: %.1fs.\n", (double)(end-start)/CLOCKS_PER_SEC);
-	return 0;
 }
 
 
