62a6fa7641d2e582a0a20a01b5a3004e1e7ca097
hxchi <hxchi@outlook.com>
2018-02-27 15:31:49 +0800

    practice 5, add set.

--- ex11_03.cpp
+++ ex11_03.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <set>
 
 using std::cin; using std::cout; using std::endl;
-using std::string; using std::map;
+using std::string; using std::map; using std::set;
 
 int main()
 {
+  set<string> exclude = {"the", "but", "is", "a", "an"};
   map<string, size_t> word_count;
   string word;
   while(cin >> word){
-    word_count[word]++;
+    if(exclude.find(word) == exclude.end()){
+      word_count[word]++;
+    }
   }
 
   // 打印测试结果
   for(const auto &w : word_count){
     cout << w.first << " occurs " << w.second
-	 << ((w.second > 1) ? "times" : "time") << endl;
+	 << ((w.second > 1) ? " times" : " time") << endl;
   }
 
   return 0;
