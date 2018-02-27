df061bec916ace941b173cc5a0470a4c43aa9c58
hxchi <hxchi@outlook.com>
2018-02-27 10:44:09 +0800

    practice 4, add isShorter

--- ex10_09.cpp
+++ ex10_09.cpp
@@ -6,6 +6,8 @@
 using std::cin; using std::cout; using std::endl;
 using std::string; using std::vector; 
 
+bool isShorter(const string &s1, const string &s2);
+
 int main()
 {
   vector<string> vs{"the", "quick", "red", "fox", "jumps", "over", "the", "red", "turtle"};
@@ -14,6 +16,7 @@ int main()
   auto end_unique = unique(vs.begin(), vs.end());
   vs.erase(end_unique, vs.end());
 
+  stable_sort(vs.begin(), vs.end(), isShorter);
   // 测试
   for(auto i : vs){
     cout << i << " ";
@@ -21,4 +24,9 @@ int main()
   cout << endl;
   
   return 0;
+}
+
+bool isShorter(const string &s1, const string &s2)
+{
+  return s1.size() < s2.size();
 }
