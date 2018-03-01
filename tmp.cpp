39e03a58bd580e24d84d17eead631c42569060fb
hxchi <hxchi@outlook.com>
2018-03-01 21:35:42 +0800

    tmp...

--- tmp.cpp
+++ tmp.cpp
@@ -1,9 +1,11 @@
 // c
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // cpp
 #include <iostream>
+#include <string>
 #include <vector>
 #include <set>
 #include <algorithm>
@@ -11,12 +13,19 @@
 using std::cin; using std::cout; using std::endl;
 using std::vector; using std::set; using std::multiset;
 using std::min_element; using std::max_element;
+using std::string; 
 
 int func(const char *x);
 
 int main()
 {
-  cout << func("hxc") << endl;
+  char ch[10] = "value";
+  char * ch1 = ch;
+  cout << sizeof(ch) << endl;	// 输出10
+  cout << sizeof(ch1) << endl;	// 输出8
+  cout <<  endl;
+  cout << strlen(ch) << endl;	// 输出5
+  cout << strlen(ch1) << endl;	// 输出5
   
   return 0;
 }
