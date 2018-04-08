2777502f3b72d37414d1b7dd08e3ff8dd0cf643f
hxchi <hxchi@outlook.com>
2018-04-08 08:54:09 +0800

    记忆力游戏

---
+++ yuc/rember.cpp
@@ -0,0 +1,92 @@
+// 记忆力游戏
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+
+#define MAXSTAGE 10
+#define MIMLEVEL 3
+#define MAXLEVEL 20
+
+// 系统暂停x毫秒
+int sleep(unsigned long x) 
+{
+  clock_t c1 = clock(), c2;
+  do {
+    if ((c2 = clock()) == (clock_t)-1) 
+      return 0;
+  } while (1000.0 * (c2-c1) / CLOCKS_PER_SEC < x);
+  return 1;
+}
+
+
+void getnumber(int level, char a[42])
+{
+  int c;
+  for(int i = 0; i < level; i++) {
+    if (i == 0) {
+      c = (rand()%9) + 1;
+      sprintf(&a[0], "%d", c);
+    }
+    else {
+      c = rand()%10;
+      sprintf(&a[i], "%d", c);
+    }
+  }
+}
+
+
+/* ....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo...... */
+// 主函数
+int main()
+{
+  clock_t	start, end; 
+  srand(time(NULL)); 
+
+  char a[42];
+  char b[42];
+  int c;
+  int m = 0,n = 0;
+
+  start = clock();
+  while (1) {
+    m++;
+    printf("choose a level from 3 to 20: ");
+    scanf("%d", &c);
+    getnumber(c, a);
+    printf("%s", a);
+    fflush(stdout);
+    sleep(1000);
+
+    printf("\rnow please enter you answer: ");
+    scanf("%s", b);
+    printf("%s\n", b);
+    if (strcmp(a, b) == 0) {
+      printf("you are right...\n");
+      n++;
+    }
+    if (m == 3) {
+      printf("game over...\n");
+      break;
+    }
+  }
+  end = clock();
+
+  printf("you get %d right...\n", n);
+  printf("use %.1fs...\n", (double)(end-start)/CLOCKS_PER_SEC);
+
+  // test number
+  // getnumber(20, a);
+  // for (int i = 0; i < strlen(a); i++) {
+  // 	printf("%c", a[i]);
+  // }
+  // printf("\n");
+	
+  return 0;
+}
+
+
+
+
